b4f1184f937dfea9a1130a7f97ba2d2a0996ca77
kricsleo <kricsleo@163.com>
2025-04-20 19:27:28 +0800

    case

---
+++ src/features/settings/Settings2.vue
@@ -0,0 +1,85 @@
+<script setup lang="ts">
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '~/components/ui/select'
+import { Input } from '~/components/ui/input'
+import { useSettings } from './settings';
+import { useModels } from '../ai/models'
+import { providers } from '../ai/providers'
+import { ref } from 'vue';
+import Langs from './langs/Langs.vue';
+
+const { provider, apiKey, model } = useSettings()
+const { models, loading } = useModels()
+
+const apiKeyMasked = ref(true)
+</script>
+
+<template>
+  <div class="space-y-6">
+
+    <fieldset class="flex items-center gap-4">
+      <label class="w-18 text-right shrink-0 text-muted-foreground">Provider</label>
+      <Select v-model="provider">
+        <SelectTrigger class="grow-1">
+          <SelectValue placeholder="Select Provider" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem v-for="provider in providers" :key="provider.value" :value="provider.value">
+            {{ provider.label }}
+          </SelectItem>
+        </SelectContent>
+      </Select>
+    </fieldset>
+
+    <fieldset class="flex items-center gap-4">
+      <label class="w-18 text-right shrink-0 text-muted-foreground">API Key</label>
+      <div class="grow-1 relative flex items-center">
+        <Input 
+          class="grow-1 pr-10" 
+          :type="apiKeyMasked ? 'password' : 'text'" 
+          placeholder="Enter API key" 
+          v-model="apiKey" />
+        <span 
+          class="absolute right-0 h-full w-10 flex items-center justify-center"
+          @click="apiKeyMasked = !apiKeyMasked">
+          <i :class="apiKeyMasked ? 'icon-[hugeicons--view-off]' : 'icon-[hugeicons--view]'" />
+        </span>
+      </div>
+    </fieldset>
+
+    <fieldset class="flex items-center gap-4">
+      <label class="w-18 text-right shrink-0 text-muted-foreground">Model</label>
+      <Select v-model="model">
+        <SelectTrigger class="grow-1">
+          <SelectValue placeholder="Select Model" />
+        </SelectTrigger>
+        <SelectContent>
+          <template v-if="models.length">
+            <SelectItem v-for="model in models" :key="model.name" :value="model.name">
+              {{ model.displayName }}
+            </SelectItem>
+          </template>
+          <SelectItem 
+            v-else-if="loading" 
+            disabled 
+            value="loading"> 
+            Loading models...
+          </SelectItem>
+          <SelectItem 
+            v-else 
+            disabled 
+            value="not-avaliable"> 
+            No models available 
+          </SelectItem>
+        </SelectContent>
+      </Select>
+    </fieldset>
+
+    <Langs />
+  </div>
+</template>
