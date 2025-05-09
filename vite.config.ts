3cfde0d1a27b455b5ae5e4b805c8e068e3ac17b2
kricsleo <kricsleo@163.com>
2025-05-09 20:25:13 +0800

    feat: add bundle analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from "@tailwindcss/vite"
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   plugins: [
     vue(),
     tailwindcss(),
+    visualizer(),
   ],
   resolve: {
     alias: {
