8f50c9c6f5df721dcc2a8dcda3e2adeb350e084a
kricsleo <kricsleo@163.com>
2025-05-13 15:50:23 +0800

    chore: diable running analyzer in CI

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from "@tailwindcss/vite"
+import { analyzer } from 'vite-bundle-analyzer'
 
 export default defineConfig({
   plugins: [
     vue(),
     tailwindcss(),
+    analyzer(),
   ],
   resolve: {
     alias: {
