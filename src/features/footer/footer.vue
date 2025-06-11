fc6621f6a1c7aadcb2e4bcce2828394c6bb28a66
kricsleo <kricsleo@163.com>
2025-06-11 23:02:47 +0800

    refactor: replace github icon

--- src/features/footer/footer.vue
+++ src/features/footer/footer.vue
@@ -3,9 +3,9 @@ const year = new Date().getFullYear();
 </script>
 
 <template>
-  <footer class="flex items-center justify-center py-4 text-muted-foreground text-sm opacity-45">
+  <footer class="flex justify-center py-4 text-muted-foreground text-sm opacity-45">
     <a
-      class="flex items-center hover:underline underline-offset-4 mr-1"
+      class="hover:text-foreground transition-colors mr-1"
       href="https://github.com/kricsleo" 
       target="_blank" 
       rel="noopener noreferrer">
@@ -13,11 +13,10 @@ const year = new Date().getFullYear();
     </a>
     <span>{{ year }} MIT License</span>
     <a
-      class="flex items-center ml-6"
+      class="ml-8 text-lg hover:text-foreground transition-colors iconify hugeicons--github-01"
       href="https://github.com/kricsleo/ai-translator" 
       target="_blank" 
       rel="noopener noreferrer">
-      <i class="iconify hugeicons--github" />
     </a>
   </footer>
 </template>
